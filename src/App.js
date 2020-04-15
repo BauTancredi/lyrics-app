507c29180a6cfc378ec753b47baa01dc39f6e515
BauTancredi <bautistatancredi@hotmail.com>
2020-04-14 21:28:21 -0300

    Song component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import Form from "./components/Form";
+import Song from "./components/Song";
 
 function App() {
+  const [searchLyric, setsSearchLyric] = useState({});
+  const [lyric, setLyric] = useState("");
+
+  useEffect(() => {
+    if (Object.keys(searchLyric).length === 0) return;
+
+    const fetchAPILyric = async () => {
+      const { artist, song } = searchLyric;
+      const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+      const result = await axios(url);
+      setLyric(result.data.lyrics);
+    };
+    fetchAPILyric();
+  }, [searchLyric]);
+
   return (
     <Fragment>
-      <Form />
+      <Form setsSearchLyric={setsSearchLyric} />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Song lyric={lyric} />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = ({ setsSearchLyric }) => {
+  const [search, setSearch] = useState({
+    artist: "",
+    song: "",
+  });
+  const [error, setError] = useState(false);
+  const { artist, song } = search;
+
+  const handleChange = (e) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (artist.trim() === "" || song.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+    setsSearchLyric(search);
+  };
+
   return (
     <div className="bg-info">
+      {error ? (
+        <p className="alert alert-danger text-center p-2">
+          All fields are mandatory.
+        </p>
+      ) : null}
       <div className="container">
         <div className="row">
           <form
             action=""
             className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+            onSubmit={handleSubmit}
           >
             <fieldset>
               <legend className="text-center">Search Songs Lyrics</legend>
@@ -21,6 +53,8 @@ const Form = () => {
                       className="form-control"
                       name="artist"
                       placeholder="Artist Name"
+                      onChange={handleChange}
+                      value={artist}
                     />
                   </div>
                 </div>
@@ -32,6 +66,8 @@ const Form = () => {
                       className="form-control"
                       name="song"
                       placeholder="Song Name"
+                      onChange={handleChange}
+                      value={song}
                     />
                   </div>
                 </div>
